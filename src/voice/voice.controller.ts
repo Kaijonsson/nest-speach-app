0b6d528cc37377d2bbe441b2536ddcbc07682436
11Korvar <Axel.jonsson@hotmail.com>
2023-08-24 20:34:06 +0200

    com with openai working

--- src/voice/voice.controller.ts
+++ src/voice/voice.controller.ts
@@ -1,38 +1,22 @@
-import {
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { VoiceService } from './voice.service';
 
 @Controller('voice')
 export class VoiceController {
-  private readonly voiceService: VoiceService;
-  constructor(voiceService: VoiceService) {
-    this.voiceService = voiceService;
-  }
+  constructor(private readonly voiceService: VoiceService) {}
 
-  @Post('upload')
-  @UseInterceptors(
-    FileInterceptor(
-      'file',
-      //Specifies the destination folder for where to store the received file.
-      { dest: './uploads' },
-    ),
-  )
-  async uploadAudioFile(
-    @UploadedFile()
-    file: Express.Multer.File,
-  ): Promise<unknown> {
+  @Post('interact')
+  async interactWithChat(
+    @Body() body: { recognizedText: string },
+  ): Promise<any> {
     try {
-      console.log('file--> ', file);
-      const transcription = await this.voiceService.transcribe(file);
-      return { status: 'ok' };
-      // return transcription;
-    } catch (err) {
-      return { error: 'error' };
+      const response = await this.voiceService.interactWithChat(
+        body.recognizedText,
+      );
+      return { status: 'ok', response };
+    } catch (error) {
+      console.error('Error during interaction:', error);
+      return { error: 'Interaction failed' };
     }
   }
 }
--- src/voice/voice.service.ts
+++ src/voice/voice.service.ts
@@ -1,45 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { createReadStream } from 'fs';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 @Injectable()
 export class VoiceService {
-  public async transcribe(audioFile: Express.Multer.File): Promise<unknown> {
+  public async interactWithChat(recognizedText: string): Promise<any> {
     try {
       const API_KEY = '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';
-      const configuration = new Configuration({
+      console.log('text_-----> ', recognizedText);
+      const openai = new OpenAI({
         organization: 'org-nd14id8uROZWx6TkgWUuiGED',
         apiKey: API_KEY,
       });
-      const openai = new OpenAIApi(configuration);
-      const response = await openai.listEngines();
-      // const audioBuffer = await this.readFileAsBuffer(audioFile);
-      // const audioStream = this.createReadableStream(audioFile);
-      console.log('audiofile: ', audioFile);
-      const audioStream = createReadStream(audioFile.path);
-      const resp = await openai.createTranscription(
-        //FIXME: bad any
-        audioStream as any,
-        'whisper-1',
-      );
-      console.log(JSON.stringify('data', resp.request.data));
-      return resp;
-    } catch (err) {
-      //error object on whisper endpoint.
-      console.error(err?.response?.data);
-      throw err;
+
+      const response = await openai.completions.create({
+        model: 'text-davinci-003',
+        prompt:
+          'give me a recipy from these ingredients: eggs, bacon, potatoes, cream, pasta',
+      });
+      console.log('gpt response: ', response);
+
+      return response;
+    } catch (error) {
+      console.error('Error from OpenAI:', error);
+      throw error;
     }
   }
-
-  // private async readFileAsBuffer(file: Blob): Promise<Buffer> {
-  //   const arrayBuffer = await file.arrayBuffer();
-  //   return Buffer.from(arrayBuffer);
-  // }
-
-  // private createReadableStream(buffer: Buffer): Readable {
-  //   const readableStream = new Readable();
-  //   readableStream.push(buffer);
-  //   // readableStream.push(null);
-  //   return readableStream;
-  // }
 }
